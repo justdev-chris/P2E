1db465a49c2c05bd886bf4ac90b9463218cbf4dd
Chris <morenochristopher580@gmail.com>
2025-08-16 18:55:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-print("if u see this that means u installed this file from artifacts in MY REPO. please fork this repo and add your python code to "app.py" replace the existing python code with yours")
+print("if u see this that means u installed this file from artifacts in MY REPO. please fork this repo and add your python code to "app.py" replace the existing python code with yours"),
