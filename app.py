bfec28128be589c0b9ea58e8810f63acb4e08f22
Chris <morenochristopher580@gmail.com>
2025-08-23 15:44:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 def main():
-    print("If you see this, you downloaded this from the ORIGINAL repo and not YOUR forked repo, OR you did not change the python code in "app.py" with your code which is why your seeing this.")
-    input("Press enter to continue...")
+    print("Hello from your lil catboy program!")
+    input("Press Enter to close...")
 
-if __name__ == "__main__":
-    main() 
-    
+main()
