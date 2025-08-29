4d5551f6763c686db38198661391c14fbe0f893e
Chris <morenochristopher580@gmail.com>
2025-08-29 13:36:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ def main():
     # custom title for the window
     window = webview.create_window(
         title="Ultimate Cat Clicker",
-        url="https://catsdevs.online/Ultimate-Cat-Clicker/uccv3-CHAPTER2/",  # you can change this to any site or local file
+        url="https://catsdevs.online/Ultimate-Cat-Clicker/UCC/", 
         fullscreen=True
     )
 
-    # start the webview loop
     webview.start()
 
 if __name__ == "__main__":
