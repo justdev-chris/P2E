b25c0509fdc99745493210e7c1da7645f68be133
Chris <morenochristopher580@gmail.com>
2025-12-27 13:15:20 -0600

    Change window title and URL to Spotify

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import webview # ...
 
 def main():
-    window = webview.create_window('tf2', 'https://play.geforcenow.com/games?game-id=40512534-ad27-4a12-afa7-6fc412288072&lang=en_US&asset-id=01_547b2064-13ad-4ba5-b928-d2770accddf1', width=800, height=600)
+    window = webview.create_window('Spotify', 'https://open.spotify.com/', width=800, height=600)
     webview.start()
 
 if __name__ == '__main__':
