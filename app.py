f6145d7575075486bd3e856619ae85d8ced84980
Chris <morenochristopher580@gmail.com>
2025-12-27 13:22:02 -0600

    Change Spotify URL to GeForce Now URL

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import webview # ...
 
 def main():
-    window = webview.create_window('Spotify', 'https://open.spotify.com/', width=800, height=600)
+    window = webview.create_window('Spotify', 'https://play.geforcenow.com/mall/', width=800, height=600)
     webview.start()
 
 if __name__ == '__main__':
