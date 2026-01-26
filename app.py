18f16771f7d019b1a0a5b1719b64fb3e7b0439c5
Chris <morenochristopher580@gmail.com>
2026-01-25 23:10:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-# Save as install.py
-import os, shutil, sys
 
-# Auto-install without prompts
-exe = 'pycat.exe'
-dest = os.path.join(os.environ['WINDIR'], 'System32', 'pycat.exe')
-
-if os.path.exists(exe):
-    try:
-        shutil.copy(exe, dest)
-        print(f"Installed! Use: pycat --help")
-    except:
-        print("Run as Admin!")
-else:
-    print(f"Place {exe} next to this script")
-input()
